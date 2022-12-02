a5da4ea5e8985d9091a512805d52aacc355795bf
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2022-12-01 22:16:56 -0500

    Implement health transfer

--- src/main/java/com/github/tatercertified/lifesteal/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/com/github/tatercertified/lifesteal/mixin/ServerPlayerEntityMixin.java
@@ -9,16 +9,21 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.encryption.PlayerPublicKey;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
 
 //NOTICE: file was modified to use gamerules instead
 //of the configuration implementation and to use attributes, it also fixes the max health attribute
@@ -26,6 +31,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
+
+	@Shadow @Final public ServerPlayerInteractionManager interactionManager;
 
 	public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile, @Nullable PlayerPublicKey publicKey) {
 		super(world, pos, yaw, gameProfile, publicKey);
@@ -46,7 +53,7 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
 		ServerWorld world = player.getWorld();
 		Entity entity = source.getAttacker();
 		int stealAmount = world.getGameRules().getInt(Loader.STEALAMOUNT);
-		if(entity instanceof ServerPlayerEntity) {
+		if(entity instanceof ServerPlayerEntity && !Objects.equals(player.getIp(), "127.0.0.1")) {
 			updateValueOf((ServerPlayerEntity)entity, stealAmount);
 			updateValueOf(player, -stealAmount);
 		} else if(!world.getGameRules().getBoolean(Loader.PLAYERRELATEDONLY)) {
