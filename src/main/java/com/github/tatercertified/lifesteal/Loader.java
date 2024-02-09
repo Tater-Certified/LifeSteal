13d193fed622a2a25d93017f29938ae585356673
Ampflower <ampflower@outlook.com>
2024-02-09 08:12:39 -0600

    Reintroduce bare player NBT access for legacy players

--- src/main/java/com/github/tatercertified/lifesteal/Loader.java
+++ src/main/java/com/github/tatercertified/lifesteal/Loader.java
@@ -5,9 +5,7 @@ import com.github.tatercertified.lifesteal.command.AdminCommand;
 import com.github.tatercertified.lifesteal.command.GiftCommand;
 import com.github.tatercertified.lifesteal.command.WithdrawCommand;
 import com.github.tatercertified.lifesteal.item.ModItems;
-import com.github.tatercertified.lifesteal.util.Config;
-import com.github.tatercertified.lifesteal.util.OfflineUtils;
-import com.github.tatercertified.lifesteal.util.PlayerUtils;
+import com.github.tatercertified.lifesteal.util.*;
 import com.github.tatercertified.lifesteal.world.features.Ores;
 import com.github.tatercertified.lifesteal.world.gamerules.LSGameRules;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
@@ -15,7 +13,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
@@ -30,8 +27,6 @@ import static com.github.tatercertified.lifesteal.item.HeartItem.isAltar;
 public class Loader implements ModInitializer {
 
 	public static final String MOD_ID = "lifesteal";
-
-
 
 	@Override
 	public void onInitialize() {
@@ -68,22 +63,22 @@ public class Loader implements ModInitializer {
 			} else if (PlayerUtils.isPlayerDead(connecting.getUuid(), server)) {
 				connecting.changeGameMode(GameMode.SPECTATOR);
 			} else {
-				NbtCompound compound = OfflineUtils.getPlayerData(connecting.getUuid(), server, connecting.getName().getString());
-				String reviver = OfflineUtils.getReviver(compound);
-				if (!reviver.isEmpty()) {
-					postRevival(compound, connecting, reviver, server);
+				OfflinePlayerData data = OfflineUtils.getOfflinePlayerData(server, connecting.getGameProfile());
+				LifeStealPlayerData compound = data.getLifeStealData();
+				if (compound != null) {
+					postRevival(compound, connecting, server);
+					data.setLifeStealData(null);
+					data.save();
 				}
 			}
 		});
 	}
 
-	private void postRevival(NbtCompound compound, ServerPlayerEntity player, String reviver, MinecraftServer server) {
-		player.sendMessage(Text.literal((reviver + Config.REVIVER)));
-		OfflineUtils.setReviver(compound, "");
-		BlockPos pos = OfflineUtils.getLocation(compound);
+	private static void postRevival(LifeStealPlayerData data, ServerPlayerEntity player, MinecraftServer server) {
+		player.sendMessage(Text.literal((data.reviver + Config.REVIVER)));
+		BlockPos pos = data.teleport;
 		PlayerUtils.setBaseHealth(player, server.getGameRules().getInt(LSGameRules.HEARTBONUS), server);
-		FabricDimensions.teleport(player, OfflineUtils.getDimension(compound, server), new TeleportTarget(pos.toCenterPos(), Vec3d.ZERO, 0.0f, 0.0f));
+		FabricDimensions.teleport(player, data.resolveDimension(server), new TeleportTarget(pos.toCenterPos(), Vec3d.ZERO, 0.0f, 0.0f));
 		player.changeGameMode(GameMode.SURVIVAL);
-		OfflineUtils.savePlayerData(player.getUuid(), server, player.getName().getString(), compound);
 	}
 }
