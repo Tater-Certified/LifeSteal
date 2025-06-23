6ff68b6fc59138b67aa31324e6169ef1e7aca5be
d-catte <qpcrummer@gmail.com>
2025-06-23 16:29:40 -0400

    Slight tweakage

--- src/main/java/com/github/certifiedtater/lifesteal/utils/PlayerUtils.java
+++ src/main/java/com/github/certifiedtater/lifesteal/utils/PlayerUtils.java
@@ -189,13 +189,13 @@ public final class PlayerUtils {
      * @param server MinecraftServer instance
      */
     public static void convertHealthToHeartItems(ServerPlayerEntity player, int hearts, MinecraftServer server, boolean action) {
-        // Check for overflowing
-        if (hearts < 0) {
+
+        final int health = hearts * server.getGameRules().getInt(LifeStealGamerules.HEARTBONUS);
+        // Detect overflow
+        if (health < 0) {
             player.sendMessage(LifeStealText.LOW_HEALTH, action);
             return;
         }
-
-        final int health = hearts * server.getGameRules().getInt(LifeStealGamerules.HEARTBONUS);
         if(health == 0) {
             player.sendMessage(LifeStealText.HEART_DISABLED, action);
             return;
