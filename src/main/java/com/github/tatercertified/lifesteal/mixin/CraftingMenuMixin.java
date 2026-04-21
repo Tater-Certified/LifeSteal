3db8b1de6d967eea0076930c228daac0da356668
QPCrummer <qpcrummer@gmail.com>
2026-04-20 20:50:19 -0400

    Fix mixins for 26.1.x

--- src/main/java/com/github/tatercertified/lifesteal/mixin/CraftingMenuMixin.java
+++ src/main/java/com/github/tatercertified/lifesteal/mixin/CraftingMenuMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(CraftingMenu.class)
 public abstract class CraftingMenuMixin {
     @WrapOperation(method = "slotChangedCraftingGrid", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/crafting/CraftingRecipe;assemble(Lnet/minecraft/world/item/crafting/RecipeInput;)Lnet/minecraft/world/item/ItemStack;"))
-    private static ItemStack lifesteal$checkIfHeartCrafted(CraftingRecipe instance, RecipeInput recipeInput, Operation<ItemStack> original, @Local(argsOnly = true, name = "player") Player player) {
+    private static ItemStack lifesteal$checkIfHeartCrafted(CraftingRecipe instance, RecipeInput recipeInput, Operation<ItemStack> original, @Local(argsOnly = true) Player player) {
         ItemStack result = original.call(instance, recipeInput);
         if (result.is(ModItems.HEART)) {
             if (player instanceof ServerPlayer serverPlayer && ((CraftedHeartsInterface)serverPlayer).canCraftHeart()) {
--- src/main/java/com/github/tatercertified/lifesteal/mixin/ItemStackMixin.java
+++ src/main/java/com/github/tatercertified/lifesteal/mixin/ItemStackMixin.java
@@ -1,6 +1,5 @@
 package com.github.tatercertified.lifesteal.mixin;
 
-import com.github.tatercertified.lifesteal.items.HeartItem;
 import com.github.tatercertified.lifesteal.items.ModItems;
 import net.minecraft.core.TypedInstance;
 import net.minecraft.core.component.DataComponentGetter;
@@ -17,7 +16,8 @@ public interface ItemStackMixin extends TypedInstance<Item>, DataComponentGetter
     @Inject(method = "getMaxStackSize", at = @At("HEAD"), cancellable = true)
     private void lifesteal$redirectMaxStackSize(CallbackInfoReturnable<Integer> cir) {
         if (this.is(ModItems.HEART)) {
-            cir.setReturnValue(((HeartItem)this).getDefaultMaxStackSize());
+            Item item = this.typeHolder().value();
+            cir.setReturnValue(item.getDefaultMaxStackSize());
         }
     }
 }
