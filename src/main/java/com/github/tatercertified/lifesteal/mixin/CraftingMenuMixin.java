f112631046eab27c3c12f4ce59de70cf483544c5
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2026-08-11 03:00:20 -0400

    CraftingMenuMixin.java 2 electric boogaloo
    Fixed my web editing masterpiece

--- src/main/java/com/github/tatercertified/lifesteal/mixin/CraftingMenuMixin.java
+++ src/main/java/com/github/tatercertified/lifesteal/mixin/CraftingMenuMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(CraftingMenu.class)
 public abstract class CraftingMenuMixin {
     @WrapOperation(method = "slotChangedCraftingGrid", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/crafting/CraftingRecipe;assemble(Lnet/minecraft/world/item/crafting/RecipeInput;)Lnet/minecraft/world/item/ItemStack;"))
-    private static ItemStack lifesteal$checkIfHeartCrafted(CraftingRecipe instance, RecipeInput recipeInput, Operation<ItemStack> original, @Local(name = "serverPlayer") ServerPlayer player) {
+    private static ItemStack lifesteal$checkIfHeartCrafted(CraftingRecipe instance, RecipeInput recipeInput, Operation<ItemStack> original, @Local(name = "serverPlayer") ServerPlayer serverPlayer) {
         ItemStack result = original.call(instance, recipeInput);
         if (result.is(ModItems.HEART)) {
             if (((CraftedHeartsInterface) serverPlayer).canCraftHeart()) {
